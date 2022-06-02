085c32c8991e9e52d4a92d95cd4712381c759131
Yhonatan Peguero <2017-0516@unad.edu.do>
2022-06-02 15:20:09 -0400

    first commit

---
+++ Numbers.js
@@ -0,0 +1,23 @@
+//- Una variable que contenga tu altura en centímetros (entero)
+const altura = "167cm";
+console.log(altura);
+
+// - Una variable que contenga tu altura en metros (número de coma flotante)
+const height = 5.7;
+console.log(height);
+
+//- Una variable que contenga tu peso en kilogramos (número de coma flotante)
+const peso = "65kg";
+console.log(peso);
+
+//- Una variable que contenga tu altura en metros redondeada hacia arriba
+const heightRoundUp = Math.ceil(height);
+console.log(heightRoundUp);
+
+//- Una variable que contenga tu peso en kilogramos redondeado hacia abajo
+const weightRoundDown = Math.floor(peso);
+console.log(weightRoundDown);
+
+//- Una variable que contenga si "el máximo valor que se puede obtener en Javascript + 1" es igual al máximo valor que se puede obtener en Javascript
+const maxValue = Number.MAX_VALUE;
+console.log(maxValue);
